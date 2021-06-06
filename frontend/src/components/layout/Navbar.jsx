4ae9dafaeb072218d3d86d61ebab2a571c850c32
numan <numan.h.99@gmail.com>
2021-06-06 15:34:07 +0800

    logout and react private route

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,18 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { logout } from '../../actions/auth'
+import { Fragment } from 'react'
 
-const Navbar = () => {
-    return (
-        <div>
-            <nav className="navbar bg-dark">
-                <h1><Link to='/'>TygrSports</Link> </h1>
-                <ul>
-                    <li><Link to='/register'>Register</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
-                </ul>
-            </nav>
-        </div>
-    )
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className="hide-dm">Logout</span>
+        </a>
+      </li>
+    </ul>
+  )
+
+  const guestLinks = (
+    <ul>
+      <li><Link to='/register'>Register</Link></li>
+      <li><Link to='/login'>Login</Link></li>
+    </ul>
+  )
+
+  return (
+    <nav className="navbar bg-dark">
+      <h1><Link to='/'>TygrSports</Link> </h1>
+      { !loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
+    </nav>
+  )
 }
 
-export default Navbar
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.bool.isRequired,
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar)
--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR } from '../actions/types'
+import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from '../actions/types'
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -20,6 +20,7 @@ export default function(state = initialState, action) {
         user: payload
       }
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -29,6 +30,8 @@ export default function(state = initialState, action) {
       }
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT: 
       localStorage.removeItem('token');
       return {
         ...state,
